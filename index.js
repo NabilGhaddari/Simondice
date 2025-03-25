3641c01c9dc12788b570329face78f6cd03df5f2
nabilghaddari <nabilghaddari@alumnat.iesdamiahuguet.net>
2025-03-25 13:08:45 +0100

    Canvis

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-colors = ['red', 'green', 'blue' , 'orange' , 'yellow' , 'black' , 'violet' , 'pink'];
+const colors = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'violet', 'pink'];
+let colorsl = [];
 
+async function mostrar_colors(temps) {
 
-async function mostrar_colors(temps){
-    for (let i=0;i<colors.length;i++){
-        //TODO instruccions per mostrar el color. Per exemple
+    for (let i = 0; i < colors.length; i++) {
+        // Generar una nova posició aleatòria per cada color
+        let posició = Math.floor(Math.random() * colors.length);
 
-        //Crida al mètode esperar
+        // Espera el temps especificat
         await esperar(temps);
-        console.log(Math.floor(Math.random() * colors.length));
 
+        // Mostra el color aleatori
+        console.log(colors[posició]);
     }
 }
 
-function esperar(milliseconds){
+if ( colors == colors.push()) {
+
+}
+
+function esperar(milliseconds) {
     return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 }
 
+// Crida a la funció
 mostrar_colors(500);
 
