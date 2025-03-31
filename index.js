bd2254f58f5eaa0a4cc9686d3670026ad46bc9a7
nabilghaddari <nabilghaddari@alumnat.iesdamiahuguet.net>
2025-03-31 11:59:36 +0200

    Canvis 5

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const colors = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'violet', 'pink'];
 let colorsl = [];
+let jugadorpush= [];
 
 
+function inici(){
+    colorsl = [];
+    jugadorpush= [];
+    seguentronda();
+}
+function seguentronda(){
+    jugadorpush= [];
+    colorsl[colorsl.length] = colors[Math.floor(Math.random() * colors.length)];
+    console.log("Simón dice:", colorsl);
+    mostrar_colors();
+}
+function verificar(color) {
 
-function afegir_color(){
-    let posició = Math.floor(Math.random() * colors.length);
-    console.log(posició);
-    colorsl.push(colors[posició]);
-    console.log(colorsl);
+    jugadorpush[jugadorpush.length] = color;
+
+    if (jugadorpush[jugadorpush.length - 1] !== colorsl[jugadorpush.length - 1]) {
+        console.log("¡Fallaste! Reiniciando...");
+        setTimeout(inici, 2000);
+        return;
+    }
+
+    if (jugadorpush.length == colorsl.length) {
+        console.log("¡Bien hecho! Siguiente ronda...");
+        setTimeout(seguentronda, 1000);
+    }
 }
 
-
 async function mostrar_colors(temps) {
-
     for (let i = 0; i < colorsl.length; i++) {
         // Generar una nova posició aleatòria per cada color
-
-
-
         // Mostra el color aleatori
         console.log(colorsl[i]);
-
-
         // Espera el temps especificat
         await esperar(temps);
 
     }
 }
 
-
 function esperar(milliseconds) {
     return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 }
+verificar();
 
-// Crida a la funció
-//mostrar_colors(500);
 
-if (colorsl == colorsl.push()){
-    afegir_color();
-}
-afegir_color();
-afegir_color();
-afegir_color();
-afegir_color();
-afegir_color();
-afegir_color();
-afegir_color();
-afegir_color();
 
-mostrar_colors(1000);
+
+
+
 
